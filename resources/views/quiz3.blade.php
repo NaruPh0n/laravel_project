66c9370b02927e902270d52aeb9d23afe53fe264
Naruph0n <j08711104@gmail.com>
2018-08-28 12:40:44 +0700

    co

---
+++ resources/views/quiz3.blade.php
@@ -0,0 +1,233 @@
+<!-- View stored in resources/views/quiz3.blade.php -->
+<html>
+<body>
+<h1><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head><title>
+	
+ระบบจองวิชาเรียนออนไลน์ มหาวิทยาลัยราชภัฏวไลยอลงกรณ์ ในพระบรมราชูปถัมภ์
+</title><link href="CSS/cssStyle.css" rel="stylesheet" type="text/css">
+    <style type="text/css">
+        .style1
+        {
+            height: 26px;
+        }
+        .style2
+        {
+            height: 29px;
+        }
+        .style3
+        {
+            color: #0066FF;
+        }
+        </style>
+</head>
+
+<body>
+    <form name="form1" method="post" action="login.aspx" id="form1">
+<div>
+<input type="hidden" name="__VIEWSTATE" id="__VIEWSTATE" value="/wEPDwUKMjEwOTcwMDg2N2RkvbfnbftM8UZv7EWLcvK91UpY8g0=">
+</div>
+
+<div>
+
+	<input type="hidden" name="__VIEWSTATEGENERATOR" id="__VIEWSTATEGENERATOR" value="C2EE9ABB">
+	<input type="hidden" name="__EVENTVALIDATION" id="__EVENTVALIDATION" value="/wEWBALhz7ueAwLB2tiHDgLKw6LdBQLHyfnnAuVmFWVEeFhmQpCMpH7ogROEqVte">
+</div>
+    <div>
+      
+      <table width="1024" border="0" cellspacing="0" cellpadding="0">
+        <tbody><tr>
+          <td width="240" valign="top" bgcolor="#CCCCCC" class="TbStyleRight_Bottom">
+<link href="CSS/cssStyle.css" rel="stylesheet" type="text/css">
+<table width="239" border="0" cellpadding="0" cellspacing="0" bgcolor="#CCCCCC">
+  <tbody><tr>
+    <td>&nbsp;</td>
+    <td align="left">&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+    <td align="center"><img  src="css/valaya/004.png" width="133" height="139" border="0"></td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td colspan="3" class="TbStyleBottom">&nbsp;</td>
+  </tr>
+  <tr>
+    <td width="9">&nbsp;</td>
+    <td width="220" align="left">&nbsp;</td>
+    <td width="10">&nbsp;</td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+    <td>
+<div class="sidebar1">
+  <ul class="nav">
+ 
+  
+ 
+<li><a href="default.aspx">หน้าหลัก</a></li>
+
+	
+		<li><a href="login.aspx">เข้าสู่ระบบ</a></li>
+  	
+     <li><a href="searchsubject.aspx">ค้นหารายวิชาที่เปิดสอน</a></li>
+         <li><a href="http://acad.vru.ac.th/pdf_news/carenda_61/1_61_.pdf" target="_blank">กำหนดการลงทะเบียนภาคปกติ 
+             1/61</a></li>
+     <li><a href="http://acad.vru.ac.th/about_acad/staff/news/3/1.61.pdf" target="_blank">กำหนดการลงทะเบียนภาคพิเศษ 
+         1/61</a></li>
+     <li><a href="http://acad.vru.ac.th/about_acad/ac_CountStu.php" target="_blank">แบบฟอร์มสำหรับนักศึกษา</a></li>
+
+    <!-- <li><a href="samesub.aspx">ตรวจสอบรายวิชาเทียบโอน</a></li> -->
+    
+    
+    	<li><a href="resetpass.aspx">ขอรหัสผ่านใหม่</a></li>
+    
+  </ul>
+</div>
+    </td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+    <td align="left">&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+</tbody></table>
+</td>
+          <td width="784" valign="top"><table width="784" border="0" cellpadding="0" cellspacing="0">
+            <tbody><tr>
+              <td width="10">&nbsp;</td>
+              <td width="774">&nbsp;</td>
+            </tr>
+            <tr>
+              <td>&nbsp;</td>
+              <td>&nbsp;
+                
+                </td>
+            </tr>
+            <tr>
+              <td align="left">&nbsp;</td>
+              <td>&nbsp;</td>
+            </tr>
+            <tr>
+              <td align="left">&nbsp;</td>
+              <td>&nbsp;</td>
+            </tr>
+            <tr>
+              <td align="left">&nbsp;</td>
+              <td><table width="370" border="0" align="center" cellpadding="0" cellspacing="0" bordercolor="#000000" class="TbStyle1">
+                <tbody><tr>
+                  <td><table width="368" border="0" align="center" cellpadding="0" cellspacing="0">
+                    <tbody><tr>
+                      <td colspan="3" align="center" bgcolor="#FFCCFF" class="TbStyleBottom" height="40"><label class="Blue14Bold">เข้าสู่ระบบ</label></td>
+                    </tr>
+                    <tr>
+                      <td align="right" class="DefaultBold">&nbsp;</td>
+                      <td>&nbsp;</td>
+                      <td align="left" class="DefaultBold">&nbsp;</td>
+                    </tr>
+                    <tr>
+                      <td align="right" class="DefaultBold">รหัสนักศึกษา :</td>
+                      <td>&nbsp;</td>
+                      <td align="left" class="DefaultBold"><input name="txtUser" type="text" maxlength="11" id="txtUser" class="txtStyleH30" style="width:150px;"></td>
+                    </tr>
+                    <tr>
+                      <td align="right" class="DefaultBold">&nbsp;</td>
+                      <td>&nbsp;</td>
+                      <td align="left" class="DefaultBold">&nbsp;</td>
+                    </tr>
+                    <tr>
+                      <td align="right" class="DefaultBold">รหัสผ่าน :</td>
+                      <td>&nbsp;</td>
+                      <td align="left" class="DefaultBold"><input name="txtPass" type="password" id="txtPass" class="txtStyleH30" style="width:150px;"></td>
+                    </tr>
+                    <tr>
+                      <td align="right" class="DefaultBold">&nbsp;</td>
+                      <td>&nbsp;</td>
+                      <td align="left" class="DefaultBold">&nbsp;</td>
+                    </tr>
+                    <tr>
+                      <td colspan="3" align="center" class="DefaultBold">
+                          <input type="submit" name="btLogin" value="เข้าสู่ระบบ" id="btLogin" class="BtStyleH30" style="cursor:pointer">              
+                        &nbsp;
+                          <input id="Reset1" class="BtStyleH30" type="reset" value="ยกเลิก" style="cursor:pointer"></td>
+                    </tr>
+                    <tr>
+                      <td width="140" align="right" class="DefaultBold">&nbsp;</td>
+                      <td width="10">&nbsp;</td>
+                      <td width="218" align="left" class="DefaultBold">&nbsp;</td>
+                    </tr>
+                  </tbody></table></td>
+                </tr>
+              </tbody></table></td>
+            </tr>
+            <tr>
+              <td align="left">&nbsp;</td>
+              <td>&nbsp;</td>
+            </tr>
+            <tr>
+              <td align="left" class="style1"></td>
+              <td class="style1" style="text-align: center">* <span class="style3">รหัสผ่านใช้ 
+                  รหัสบัตรประชาชน 13 หลัก พิมพ์ติดต่อกัน ไม่ต้องใส่เครื่องหมาย -<br>
+                  ถ้าใช้รหัสบัตรประชาชนแล้วยังเข้าไม่ได้ให้ ใช้รหัสผ่านเป็น 1234 </span></td>
+            </tr>
+             <tr>
+              <td align="left" class="style1"></td>
+              <td class="style1" style="text-align: center"><!-- * <span class="style3">หรือถ้าใช้รหัสบัตรประชาชน 13 หลัก 
+                  แล้วมีข้อความแจ้งว่า ระหัสไม่ถูกต้องให้เปลี่ยนมาใส่ 1234 ตรงรหัสผ่าน-->
+               <span class="Red20Bold">นักศึกษาสามารถเข้าสู่ระบบจองได้ทั้งหมด 4 ช่องทาง</span></td>
+            </tr>
+            <tr>
+              <td align="left" class="style2"></td>
+              <td class="style2" style="text-align: center">&nbsp;</td>
+            </tr>
+            <tr>
+              <td align="left" class="style2"></td>
+              <td class="style2" style="text-align: center"><a href="http://reg1.vru.ac.th/default.aspx">ดูเกรด/จองรายวิชา ช่อง1 http://reg1.vru.ac.th </a></td>
+            </tr>
+            <tr>
+              <td align="left" class="style2"></td>
+              <td class="style2" style="text-align: center"><a href="http://reg2.vru.ac.th/default.aspx">ดูเกรด/จองรายวิชา ช่อง2 http://reg2.vru.ac.th </a></td>
+            </tr>
+            <tr>
+              <td align="left" class="style2"></td>
+              <td class="style2" style="text-align: center"><a href="http://reg1.vru.ac.th/default.aspx">ดูเกรด/จองรายวิชา ช่อง3 http://reg3.vru.ac.th </a></td>
+            </tr>
+            <tr>
+              <td align="left" class="style2"></td>
+              <td class="style2" style="text-align: center"><a href="http://reg4.vru.ac.th/default.aspx">ดูเกรด/จองรายวิชา ช่อง4 http://reg4.vru.ac.th </a></td>
+            </tr>
+            <tr>
+              <td align="left" class="style2"></td>
+              <td class="style2" style="text-align: center">&nbsp;</td>
+            </tr>
+          </tbody></table></td>
+        </tr>
+        <tr>
+          <td valign="top">&nbsp;</td>
+          <td valign="top">&nbsp;</td>
+        </tr>
+        <tr>
+          <td colspan="2" valign="top">&nbsp;</td>
+        </tr>
+        <tr>
+          <td valign="top">&nbsp;</td>
+          <td valign="top">&nbsp;</td>
+        </tr>
+      </tbody></table>
+    <div><link href="CSS/cssStyle.css" rel="stylesheet" type="text/css">
+
+<marquee class="Red12Bold"><!-- ระบบจองรายวิชา มหาวิทยาลัยราชภัฎวไลยอลงกรณ์ ในพระบรมราชูปถัมภ์ --></marquee>
+
+    </div>
+    
+
+
+</div></form></body>
+</html>
+
+</h1>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,51 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/home', function() {
+ 	return '<h1>This is home page</h1>' ;
+});
+
+Route::get('/blog/{page_id}', function($page_id) {
+     return "<h1>This is blog page : $page_id </h1>" ;
+});
+Route::get('/product/{a}/{b}/{c}', function($a, $b, $c) {
+ 	return "<h1>This is product page </h1><div>$a , $b, $c</div>" ;
+});
+Route::get('/category/{a?}', function($a = "mobile") {
+ 	return "<h1>This is category page : $a </h1>" ;
+});
+
+
+Route::get('/greeting', function () {
+	$data = [
+'name'      => 'James' ,
+'last_name' => 'Mars'
+];
+return view('greeting', $data);
+});
+
+Route::get('/hello', function () {	
+return view('hello');
+});
+Route::get('/combine/{id}', function ($id) {
+	$data = [
+'id' => $id
+];
+return view('combine', $data);
+});
+
+Route::get('/quiz3', function () {	
+return view('quiz3');
+});
+
+
+
+
+
+
+
+
